cc3f28ccfd33def7c5b1ffc9e908e14fb7d93f4b
MUSTACHERS <chaste86@163.com>
2018-10-15 20:23:00 +0800

    add

---
+++ src/String/StringSearch/KMP.java
@@ -0,0 +1,57 @@
+package String.StringSearch;
+
+
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * @author MUSTACHE
+ * 
+ * %java KMP AACAA AABRAACADABRAACAADABRA
+ * text:   AABRAACADABRAACAADABRA
+ * pattern:             AACAA
+ */
+public class KMP {
+	private String pat;
+	private int[][] dfa;
+	public KMP(String pat) {
+		this.pat = pat;
+		int M = pat.length();
+		int R = 256;
+		dfa = new int[R][M];
+		dfa[pat.charAt(0)][0] = 1;
+		for(int X = 0, j = 1; j < M; j++) {
+			// 计算dfa[][]
+			for(int c = 0; c < R; c++) {
+				dfa[c][j] = dfa[c][X]; // 复制匹配失败情况下的值
+			}
+			dfa[pat.charAt(j)][j] = j+1; // 设置匹配成功情况下的值
+			X = dfa[pat.charAt(j)][X]; // 更新重启状态
+		}
+	}
+	
+	public int search(String txt) {
+		int i, j, N = txt.length(), M = pat.length();
+		for(i = 0, j = 0; i < N && j < M; i++) {
+			j = dfa[txt.charAt(i)][j];
+		}
+		if(j == M) {
+			return i - M; // 找到匹配（到达模式字符串的结尾）
+		} else {
+			return N; // 未匹配（到达文本字符串的结尾）
+		}
+	}
+	
+	public static void main(String[] args) {
+		String pat = args[0];
+		String txt = args[1];
+		KMP kmp = new KMP(pat);
+		StdOut.println("text:   " + txt);
+		int offset = kmp.search(txt);
+		StdOut.print("pattern: ");
+		for(int i = 0; i < offset; i++) {
+			StdOut.print(" ");
+		}
+		StdOut.println(pat);
+	}
+}
